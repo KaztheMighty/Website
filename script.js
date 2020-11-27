769952e0fff6c6b63a80ee814373941d77cb813b
KaztheMighty <benjamin.pabian@hotmail.com>
2020-11-27 16:36:06 +1100

    Updation of old Website Code

---
+++ script.js
@@ -0,0 +1,14 @@
+// Setting up the automatic year for the footer
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
+
+
+/*const navToggle = document.querySelector(".menu-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+	links.classList.toggle("show-links");
+});
+
+*/
